6eaaad885261e2e5a99f55b92759a78d1261b1b0
TLCurd <TLCurdChiro@gmail.com>
2022-03-29 20:47:37 -0600

    add speaker_meeting instances; add associations between all 3 models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 require "ffaker"
-10.times do |speaker|
-  speaker = Speaker.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.safe_email)
-end
-20.times do |meeting|
-  meeting = Meeting.new(title: FFaker::CheesyLingo.title, agenda: "Talking about the title subject.", location:  FFaker::Address.street_address, time: FFaker::Time.datetime)
-  meeting.save
-end
+# 60.times do |speaker|
+#   speaker = Speaker.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.safe_email)
+# end
+
+# 60.times do |meeting|
+#   meeting = Meeting.new(title: FFaker::CheesyLingo.title, agenda: "Talking about the title subject.", location:  FFaker::Address.street_address, time: FFaker::Time.datetime)
+#   meeting.save
+# end
+
+# 60.times do |sm|
+#   sm = SpeakerMeeting.create(speaker_id: rand(1..60), meeting_id: rand(1..60))
+# end
